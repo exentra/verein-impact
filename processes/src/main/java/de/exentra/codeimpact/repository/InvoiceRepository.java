4e58bca1be188effa77cb3c68f1fae980027c2fb
Tim Grein <tim@4greins.de>
2022-12-14 19:48:28 +0100

    Add findByInvoiceNumber MongoDB query

--- processes/src/main/java/de/exentra/codeimpact/repository/InvoiceRepository.java
+++ processes/src/main/java/de/exentra/codeimpact/repository/InvoiceRepository.java
@@ -2,9 +2,17 @@ package de.exentra.codeimpact.repository;
 
 import de.exentra.codeimpact.model.Invoice;
 import io.quarkus.mongodb.panache.PanacheMongoRepository;
+import io.quarkus.mongodb.panache.PanacheQuery;
 
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class InvoiceRepository implements PanacheMongoRepository<Invoice> {
+
+    private static final String FIND_BY_INVOICE_NUMBER_FORMAT = "{\"invoiceNumber\": %d}";
+
+    public PanacheQuery<Invoice> findByInvoiceNumber(Long invoiceNumber){
+        return find(String.format(FIND_BY_INVOICE_NUMBER_FORMAT, invoiceNumber));
+    }
+
 }
