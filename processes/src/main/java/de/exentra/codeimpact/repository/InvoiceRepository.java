fbb064737b4436c8ec9e0d0863b1a1789c6194fe
Tim Grein <tim@4greins.de>
2022-12-14 20:13:52 +0100

    Change findByInvoiceNumber and add fuzzySearch method

--- processes/src/main/java/de/exentra/codeimpact/repository/InvoiceRepository.java
+++ processes/src/main/java/de/exentra/codeimpact/repository/InvoiceRepository.java
@@ -1,18 +1,34 @@
 package de.exentra.codeimpact.repository;
 
+import com.mongodb.client.AggregateIterable;
+import com.mongodb.client.FindIterable;
 import de.exentra.codeimpact.model.Invoice;
 import io.quarkus.mongodb.panache.PanacheMongoRepository;
-import io.quarkus.mongodb.panache.PanacheQuery;
+import org.bson.Document;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.List;
 
 @ApplicationScoped
 public class InvoiceRepository implements PanacheMongoRepository<Invoice> {
 
-    private static final String FIND_BY_INVOICE_NUMBER_FORMAT = "{\"invoiceNumber\": %d}";
+    private static final String INVOICE_NUMBER = "invoiceNumber";
+    private static final String MATCH = "$match";
+    private static final String TEXT = "$text";
+    private static final String SEARCH = "$search";
 
-    public PanacheQuery<Invoice> findByInvoiceNumber(Long invoiceNumber){
-        return find(String.format(FIND_BY_INVOICE_NUMBER_FORMAT, invoiceNumber));
+    public FindIterable<Invoice> findByInvoiceNumber(Long invoiceNumber){
+        return mongoCollection().find(new Document(INVOICE_NUMBER, invoiceNumber), Invoice.class);
     }
 
+    public AggregateIterable<Invoice> fuzzySearch(String text){
+        return mongoCollection().aggregate(aggregateTextSearch(text));
+    }
+
+    private List<Document> aggregateTextSearch(String text){
+        return List.of(
+                new Document(MATCH,
+                        new Document(TEXT,
+                                new Document(SEARCH, text))));
+    }
 }
