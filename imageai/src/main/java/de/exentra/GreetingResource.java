672c5929401458c38059789169e801724c59155b
MiggiV2 <michael@familyhainz.de>
2022-12-14 21:14:37 +0100

    :construction: Azure says: Stream closed!

--- imageai/src/main/java/de/exentra/GreetingResource.java
+++ imageai/src/main/java/de/exentra/GreetingResource.java
@@ -1,18 +1,69 @@
 package de.exentra;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import com.azure.ai.formrecognizer.documentanalysis.DocumentAnalysisClient;
+import com.azure.ai.formrecognizer.documentanalysis.DocumentAnalysisClientBuilder;
+import com.azure.ai.formrecognizer.documentanalysis.models.AnalyzeResult;
+import com.azure.ai.formrecognizer.documentanalysis.models.OperationResult;
+import com.azure.core.credential.AzureKeyCredential;
+import com.azure.core.util.BinaryData;
+import com.azure.core.util.polling.SyncPoller;
+import org.jboss.resteasy.reactive.MultipartForm;
+
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.Base64;
 
 @Path("/hello")
-public class GreetingResource
-{
+public class GreetingResource {
+    // set `<your-endpoint>` and `<your-key>` variables with the values from the Azure portal
+    private static final String endpoint = "https://codeimpact-exe-form.cognitiveservices.azure.com/";
+    private static final String key = "7d860354c9654bc7b29f89896c9b4836";
 
-    @GET
+    @POST
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello()
-    {
+    public String hello(String base64Image) {
+        // create your `DocumentAnalysisClient` instance and `AzureKeyCredential` variable
+        DocumentAnalysisClient client = new DocumentAnalysisClientBuilder()
+                .credential(new AzureKeyCredential(key))
+                .endpoint(endpoint)
+                .buildClient();
+
+        // sample document
+        try (ByteArrayInputStream stream = new ByteArrayInputStream(Base64.getDecoder().decode(base64Image))) {
+            String modelId = "prebuilt-invoice";
+            SyncPoller<OperationResult, AnalyzeResult> analyzeDocumentPoller =
+                    client.beginAnalyzeDocument(modelId, BinaryData.fromStream(stream));
+
+            AnalyzeResult analyzeResult = analyzeDocumentPoller.getFinalResult();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        //
         return "Hello from RESTEasy Reactive";
+    }
+
+    @POST
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Path("upload")
+    public Response uploadImage(@MultipartForm MultipartBody multipartBody) { // create your `DocumentAnalysisClient` instance and `AzureKeyCredential` variable
+        DocumentAnalysisClient client = new DocumentAnalysisClientBuilder()
+                .credential(new AzureKeyCredential(key))
+                .endpoint(endpoint)
+                .buildClient();
+
+        // sample document
+        String modelId = "prebuilt-invoice";
+        BinaryData data = BinaryData.fromStream(multipartBody.file);
+        SyncPoller<OperationResult, AnalyzeResult> analyzeDocumentPoller =
+                client.beginAnalyzeDocument(modelId, data);
+
+        AnalyzeResult analyzeResult = analyzeDocumentPoller.getFinalResult();
+
+        return Response.ok().build();
+
     }
 }
---
+++ imageai/src/main/java/de/exentra/MultipartBody.java
@@ -0,0 +1,13 @@
+package de.exentra;
+
+import org.jboss.resteasy.reactive.PartType;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.core.MediaType;
+import java.io.InputStream;
+
+public class MultipartBody {
+    @FormParam("file")
+    @PartType(MediaType.APPLICATION_OCTET_STREAM)
+    public InputStream file;
+}
